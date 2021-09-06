1532bb40a4f4e4bcbf0eb9cf1338870dc16f5441
hoonv <chaeefe@gmail.com>
2021-09-06 17:19:38 +0900

    add entry point
    -

--- Sources/Swifthumb/Classes/Swifthumb.swift
+++ Sources/Swifthumb/Classes/Swifthumb.swift
@@ -5,12 +5,18 @@ public class Swifthumb {
     
     
     let privateAPIManager: PrivateAPIManager
-    
+    let publicAPIManager: PublicAPIManager
+
     public init(apiKey: String, secretKey: String) {
         privateAPIManager = PrivateAPIManager(apiKey: apiKey, secretKey: secretKey)
+        publicAPIManager = PublicAPIManager()
     }
     
-    public func hello() {
+    public func getBalance() {
         privateAPIManager.infoAPI.balance()
+    }
+    
+    public func getAllTicker(completionHandler: @escaping (TickerResponse) -> ()) {
+        publicAPIManager.getAllTicker(completionHander: completionHandler)
     }
 }
